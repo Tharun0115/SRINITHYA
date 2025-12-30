fa4f501d528ec6590ae883a4f0d6c0961a90807d
Tharun0115 <tharun05312@gmail.com>
2025-12-30 08:45:28 +0530

    made changes to name strip on about us page

---
+++ tailwind-config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#1e3a8a',
+                secondary: '#d97706',
+                dark: '#111827',
+            }
+        }
+    }
+}
