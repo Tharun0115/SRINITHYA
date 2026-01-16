56a1c2b7ec81a3bd82ad116dc282acddfd6b8434
Tharun0115 <tharun05312@gmail.com>
2026-01-16 17:43:00 +0530

    centered the cards

--- js/components/product_renderer.js
+++ js/components/product_renderer.js
@@ -52,7 +52,7 @@ window.createProductCard = function(product) {
     const imageSrc = product.image.startsWith('http') ? product.image : rootPath + product.image.replace('./', '');
     const imageClass = product.imageClass || 'object-contain';
     const imageContainerClass = product.imageContainerClass || 'h-32 md:h-64';
-    const cardWrapperClass = product.cardWrapperClass || 'bg-white rounded-xl border border-gray-200 hover:border-primary hover:shadow-[0_0_20px_rgba(30,58,138,0.6)] transition-all duration-300 group flex flex-col';
+    const cardWrapperClass = product.cardWrapperClass || 'bg-white rounded-xl border border-gray-200 hover:border-primary hover:shadow-[0_0_20px_rgba(30,58,138,0.6)] transition-all duration-300 group flex flex-col w-full max-w-sm mx-auto';
 
     return `
         <div class="${cardWrapperClass}" ${compareDataAttributes}>
